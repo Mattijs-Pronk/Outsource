f9f3c88f9fff395465b98c709a11f7ad68464a30
Mattijs Pronk <456410@student.fontys.nl>
2022-04-14 11:20:43 +0200

    Vaardigheden classes added

---
+++ BusinessLogicLayer/Vaardigheid.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccesLayer;
+
+namespace BusinessLogicLayer
+{
+    class Vaardigheid
+    {
+        public int Id { get; }
+
+        public string Vaardigheden { get; set; }
+
+        public Vaardigheid(VaardigheidDTO vaardigheid)
+        {
+            this.Id = vaardigheid.Id;
+            this.Vaardigheden = vaardigheid.Vaardigheid;
+        }
+    }
+}
